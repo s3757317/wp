2dd770b1c2ab46b833f2ea900290e0ff4ac52308
s3757317 <s3757317@users.noreply.github.com>
2020-04-21 15:27:53 +0700

    Week 7

---
+++ Week 7/tools.js
@@ -0,0 +1,35 @@
+function calculatePrice()
+{
+    alert("Calculate Price");
+    event.stopPropagation();
+}
+
+function calculatePrice2()
+{
+    alert("Calculate Price 2");
+}
+
+function clickP3()
+{
+    alert("P3 clicked");
+}
+
+var myButton = document.getElementById("price2");
+myButton.onclick = calculatePrice2;
+
+var myButton = document.getElementById("price3");
+myButton.addEventListener("click", calculatePrice);
+myButton.addEventListener("click", calculatePrice2);
+
+var myP3 = document.getElementById("p3");
+myP3.addEventListener("click", clickP3);
+
+function checkInput()
+{
+    var patt = /^Steve [a-zA-Z ]+$/;
+    name = document.getElementById("text1").value;
+    if (patt.test(name))
+        alert("Good")
+    else
+        alert("Bad")
+}
